6328820775bfc29890d4bd38afb39c0d57f01686
Gustavo Adolfo Mestra Garay <gmestra@grupoasd.com.co>
2020-12-22 07:39:51 -0500

    Inicial

---
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
+  }
+}
